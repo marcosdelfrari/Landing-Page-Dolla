ac3119e9b3701bdc47c71dd484fdec6b38c380d2
Marcos Lucas <afk.marcos@gmail.com>
2023-08-25 21:57:29 -0300

    created desktop version home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  isMobile: boolean | undefined;
 
+
+  constructor() {
+    this.checkScreenWidth(); // Verifique o tamanho da tela inicialmente
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.checkScreenWidth(); // Verifique o tamanho da tela sempre que a janela for redimensionada
+  }
+
+  checkScreenWidth() {
+    this.isMobile = window.innerWidth <= 768; // Defina a largura de acordo com sua definição de "mobile"
+  }
 }
